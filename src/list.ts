4cd6bd50f59b2670f3a9c0e37a7b96342de2de9f
Fabio Iassia <fbiassia@gmail.com>
2023-05-28 20:48:11 -0300

    chore: clean up

--- src/list.ts
+++ src/list.ts
@@ -1,16 +1,13 @@
 export default class List {
-  static rotate(list: Array<any> = []): Array<any> {
-    return list.slice(1, list.length).concat(list.slice(0, 1));
-  }
-
+  static rotate = (list: Array<any> = []): Array<any> =>
+     list.slice(1, list.length).concat(list.slice(0, 1));
   static lockedRotate(
     list: Array<any> = [],
     lockedPosition: number = 0
   ): Array<any> {
-    const rotatedList = [...list];
-    const [lockedItem] = rotatedList.splice(lockedPosition, 1);
-    const rotatedItems = this.rotate(rotatedList);
-    rotatedItems.splice(lockedPosition, 0, lockedItem);
-    return rotatedItems;
+    const [lockedItem] = list.splice(lockedPosition, 1);
+    const rotatedList = this.rotate(list);
+    rotatedList.splice(lockedPosition, 0, lockedItem);
+    return rotatedList;
   }
 }
