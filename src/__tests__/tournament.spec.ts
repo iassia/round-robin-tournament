93edc7e58b44030e17c37a161301a311f22a87eb
Fabio Iassia <fbiassia@gmail.com>
2023-06-03 21:48:49 -0300

    fix: prevent duplicated matches

--- src/__tests__/tournament.spec.ts
+++ src/__tests__/tournament.spec.ts
@@ -22,13 +22,12 @@ describe("Tournament", () => {
     );
   });
 
-  it("return correct first match", () => {
-    const [away, home] = TEAMS;
-    const [[firstMatch]] = tournament.matches;
-    expect(firstMatch).toEqual([home, away]);
+  it("returns correct first match", () => {
+    expect(tournament.matches)
+      .toStrictEqual([[[{"id":2,"name":"Chelsea"},{"id":1,"name":"Arsenal"}],[{"id":6,"name":"West Ham"},{"id":3,"name":"Liverpool"}],[{"id":5,"name":"Manchester United"},{"id":4,"name":"Manchester City"}]],[[{"id":1,"name":"Arsenal"},{"id":3,"name":"Liverpool"}],[{"id":4,"name":"Manchester City"},{"id":2,"name":"Chelsea"}],[{"id":5,"name":"Manchester United"},{"id":6,"name":"West Ham"}]],[[{"id":4,"name":"Manchester City"},{"id":1,"name":"Arsenal"}],[{"id":3,"name":"Liverpool"},{"id":5,"name":"Manchester United"}],[{"id":2,"name":"Chelsea"},{"id":6,"name":"West Ham"}]],[[{"id":1,"name":"Arsenal"},{"id":5,"name":"Manchester United"}],[{"id":6,"name":"West Ham"},{"id":4,"name":"Manchester City"}],[{"id":2,"name":"Chelsea"},{"id":3,"name":"Liverpool"}]],[[{"id":6,"name":"West Ham"},{"id":1,"name":"Arsenal"}],[{"id":5,"name":"Manchester United"},{"id":2,"name":"Chelsea"}],[{"id":4,"name":"Manchester City"},{"id":3,"name":"Liverpool"}]],[[{"id":1,"name":"Arsenal"},{"id":2,"name":"Chelsea"}],[{"id":3,"name":"Liverpool"},{"id":6,"name":"West Ham"}],[{"id":4,"name":"Manchester City"},{"id":5,"name":"Manchester United"}]],[[{"id":3,"name":"Liverpool"},{"id":1,"name":"Arsenal"}],[{"id":2,"name":"Chelsea"},{"id":4,"name":"Manchester City"}],[{"id":6,"name":"West Ham"},{"id":5,"name":"Manchester United"}]],[[{"id":1,"name":"Arsenal"},{"id":4,"name":"Manchester City"}],[{"id":5,"name":"Manchester United"},{"id":3,"name":"Liverpool"}],[{"id":6,"name":"West Ham"},{"id":2,"name":"Chelsea"}]],[[{"id":5,"name":"Manchester United"},{"id":1,"name":"Arsenal"}],[{"id":4,"name":"Manchester City"},{"id":6,"name":"West Ham"}],[{"id":3,"name":"Liverpool"},{"id":2,"name":"Chelsea"}]],[[{"id":1,"name":"Arsenal"},{"id":6,"name":"West Ham"}],[{"id":2,"name":"Chelsea"},{"id":5,"name":"Manchester United"}],[{"id":3,"name":"Liverpool"},{"id":4,"name":"Manchester City"}]]]);
   });
 
-  it("return 10 rounds when receive 6 teams", () => {
+  it("returns 10 rounds when receive 6 teams", () => {
     expect(tournament.matches.length).toBe(10);
   });
 });
